5216d14a9af71986be8f76779cd73a6b931dce31
Kevin Fernando Pozo Maldonado <kevinferpozo@gmail.com>
2026-01-03 18:43:18 -0500

    Componentes Mapa y Filtro Grupal Completos

--- src/views/PaginaResultadosExtranjero.vue
+++ src/views/PaginaResultadosExtranjero.vue
@@ -1,17 +1,17 @@
 <!-- src/views/PaginaResultadosExtranjero.vue -->
 <template>
   <div>
-    <MapaExtranjero />
-    
+    <MapaNacional />
   </div>
 </template>
 
 <script>
+import MapaNacional from "@/components/MapaNacional.vue";
 import MapaExtranjero from "@/components/MapaExtranjero.vue";
-
 export default {
   name: "PaginaResultadosExtranjero",
   components: {
+    MapaNacional,
     MapaExtranjero,
   },
 };
